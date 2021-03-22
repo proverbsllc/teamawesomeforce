64e6b9aae58a6368810caa2fe7638f8fad99f421
amaxim-uf <70339344+amaxim-uf@users.noreply.github.com>
2021-03-22 17:01:02 -0400

    Updated interface to include advanced and basic Working on gathering rankings from Google scholar for authors, publishers and articles.

--- content.js
+++ content.js
@@ -9,17 +9,74 @@ chrome.runtime.onMessage.addListener(
 			console.clear();
 			var authorElements = Array();
 			var keywordElements = Array();
-			
+			var affiliationElements = Array();
+			var articleElements = Array();  // 0 = paper title, 1 = publication title, 2 = publication type, 3 = abstract
+
 			console.log('----------AUTHOR LINK ELEMENTS----------');
 
 			$("meta[property='article:author']").each(function (index) 
 			{
-				var authList = $(this).attr('content').split(', ');
+				var authList = $(this).attr('content').split(',');
 				for (var i = 0; i < authList.length; i++) {
 					console.log(i + ": " + authList[i]);
-					authorElements.push(authList[i]);
+					var inList = false;
+					for (var j = 0; inList == false && j < authorElements.length; j++) {
+						if (authList[i].trim() == authorElements[j]) {
+							inList = true;
+						}
+					}
+					if (!inList) {
+						authorElements.push(authList[i].trim());
+					}
 				}
 			});
+
+			$("meta[name='citation_author']").each(function (index) {
+				var authList = $(this).attr('content').split(',');
+				for (var i = 0; i < authList.length; i++) {
+					console.log(i + ": " + authList[i]);
+					var inList = false;
+					for (var j = 0; inList == false && j < authorElements.length; j++) {
+						if (authList[i].trim() == authorElements[j]) {
+							inList = true;
+						}
+					}
+					if (!inList) {
+						authorElements.push(authList[i].trim());
+					}
+				}
+			});
+
+			$("meta[name='dc.Creator']").each(function (index) {
+				var authList = $(this).attr('content').split(',');
+				for (var i = 0; i < authList.length; i++) {
+					console.log(i + ": " + authList[i]);
+					var inList = false;
+					for (var j = 0; inList == false && j < authorElements.length; j++) {
+						if (authList[i].trim().split("  ").join(" ") == authorElements[j]) {
+							inList = true;
+						}
+					}
+					if (!inList) {
+						authorElements.push(authList[i].trim().split("  ").join(" "));
+					}
+				}
+			});
+
+			if (authorElements.length < 1) {
+				$("a[class='author-name']").each(function (index) {
+					console.log(index + ": " + $(this).attr('title'));
+					var inList = false;
+					for (var j = 0; inList == false && j < authorElements.length; j++) {
+						if ($(this).attr('title').trim().split("  ").join(" ") == authorElements[j]) {
+							inList = true;
+						}
+					}
+					if (!inList) {
+						authorElements.push($(this).attr('title').trim().split("  ").join(" "));
+					}
+				});
+			}
 
 			chrome.storage.local.set({'authorValues': authorElements}, function() 
 			{
@@ -30,11 +87,50 @@ chrome.runtime.onMessage.addListener(
 				}
 			});
 			
-			console.log('----------KEYWORD LINK ELEMENTS----------');
-			$("a[class='attributes']").each(function(index) 
-			{
-				console.log( index + ": " + $( this ).text());
-				keywordElements.push($(this).text());
+			console.log('----------KEYWORD ELEMENTS----------');
+			$("meta[name='keywords']").each(function (index) { 
+				var keyList = $(this).attr('content').split(',');
+				for (var i = 0; i < keyList.length; i++) {
+					console.log(i + ": " + keyList[i]);
+					var inList = false;
+					for (var j = 0; inList == false && j < keywordElements.length; j++) {
+						if (keyList[i].trim() == keywordElements[j]) {
+							inList = true;
+						}
+					}
+					if (!inList) {
+						keywordElements.push(keyList[i].trim());
+					}
+				}
+			});
+
+			$("meta[name='dc.Subject']").each(function (index) {
+				var keyList = $(this).attr('content').split(';');
+				for (var i = 0; i < keyList.length; i++) {
+					console.log(i + ": " + keyList[i]);
+					var inList = false;
+					for (var j = 0; inList == false && j < keywordElements.length; j++) {
+						if (keyList[i].trim() == keywordElements[j]) {
+							inList = true;
+						}
+					}
+					if (!inList) {
+						keywordElements.push(keyList[i].trim());
+					}
+				}
+			});
+
+			$("span[class='Keyword']").each(function (index) {
+				console.log(index + ": " + $(this).text());
+				var inList = false;
+				for (var j = 0; inList == false && j < keywordElements.length; j++) {
+					if ($(this).text().trim() == keywordElements[j]) {
+						inList = true;
+					}
+				}
+				if (!inList) {
+					keywordElements.push($(this).text().trim());
+				}
 			});
 
 			chrome.storage.local.set({'keywordValues': keywordElements}, function() 
@@ -45,6 +141,171 @@ chrome.runtime.onMessage.addListener(
 					console.error(error);
 				}
 			});
+
+			console.log('----------AFFILIATION ELEMENTS----------');
+			$("meta[name='citation_author_institution']").each(function (index) {
+				console.log(index + ": " + $(this).attr('content'));
+				var inList = false;
+				for (var j = 0; inList == false && j < affiliationElements.length; j++) {
+					if ($(this).attr('content').trim() == affiliationElements[j]) {
+						inList = true;
+					}
+				}
+				if (!inList) {
+					affiliationElements.push($(this).attr('content').trim());
+				}
+			});
+
+			chrome.storage.local.set({ 'affiliationValues': affiliationElements }, function () {
+				var error = chrome.runtime.lastError;
+				if (error) {
+					console.error(error);
+				}
+			});
+
+			// title
+			console.log('----------PAPER TITLE----------');
+			//articleElements = Array();  // 0 = paper title, 1 = publication title, 2 = publication type, 3 = abstract
+			var titleFound = false;
+			$("meta[name='citation_title']").each(function (index) {
+				console.log(index + ": " + $(this).attr('content'));
+				if (!titleFound) {
+					titleFound = true;
+					articleElements[0] = $(this).attr('content').trim();
+				}
+			});
+
+			if (!titleFound) {
+				$("meta[name='dc.Title']").each(function (index) {
+					console.log(index + ": " + $(this).attr('content'));
+					if (!titleFound) {
+						titleFound = true;
+						articleElements[0] = $(this).attr('content').trim();
+					}
+				});
+			}
+
+			if (!titleFound) {
+				$("h1[class='citation__title']").each(function (index) {
+					console.log(index + ": " + $(this).text());
+					if (!titleFound) {
+						titleFound = true;
+						articleElements[0] = $(this).text().trim();
+					}
+				});
+			}
+
+			console.log('----------PUBLICATION VENUE----------');
+			var pubFound = false;
+			$("meta[name='citation_conference_title']").each(function (index) {
+				console.log(index + ": " + $(this).attr('content'));
+				if (!pubFound) {
+					pubFound = true;
+					articleElements[1] = $(this).attr('content').trim();
+					articleElements[2] = "CONFERENCE PROCEEDINGS";
+				}
+			});
+
+			if (!pubFound) {
+				$("meta[name='citation_journal_title']").each(function (index) {
+					console.log(index + ": " + $(this).attr('content'));
+					if (!pubFound) {
+						pubFound = true;
+						articleElements[1] = $(this).attr('content').trim();
+						articleElements[2] = "JOURNAL ARTICLE";
+					}
+				});
+			}
+
+			if (!pubFound) {
+				$("span[class='issue-heading']").each(function (index) {
+					console.log(index + ": " + $(this).text());
+					if (!pubFound) {
+						pubFound = true;
+						articleElements[1] = "";
+						articleElements[2] = $(this).text().trim().toUpperCase();
+					}
+				});
+			}
+
+			if (!pubFound) {
+				articleElements[1] = "";
+				articleElements[2] = "";
+			}
+
+			// abstract
+			console.log('----------ABSTRACT----------');
+			var absFound = false;
+			$("meta[name='dc.Description']").each(function (index) {
+				console.log(index + ": " + $(this).attr('content'));
+				if (!absFound) {
+					absFound = true;
+					articleElements[3] = $(this).attr('content').trim();
+				}
+			});
+
+			var responseArray = Array();
+			responseArray[0] = authorElements;
+			responseArray[1] = keywordElements;
+			responseArray[2] = affiliationElements;
+
+			// 0 = paper title, 1 = publication title, 2 = publication type, 3 = abstract, 4 = pub rank
+			articleElements[4] = 0;
+			if (articleElements[1] !== undefined && articleElements.length > 1) {
+				var pubName = articleElements[1].split(" ").join("+");
+				$.get('https://scholar.google.com/citations?hl=en&view_op=search_venues&vq=' + pubName, function (response) {
+					//console.log(response);
+					console.clear();
+					var cutBy = '<td class="gsc_mvt_t">' + articleElements[1] + '</td>';
+					//console.log(cutBy);
+					var splitUp = response.split(cutBy);
+					if (splitUp.length < 2) {
+						cutBy = '<td class="gsc_mvt_t">' + articleElements[1].split("-").join(" ") + '</td>';
+						splitUp = response.split(cutBy);
+					}
+					//console.log(splitUp.length);
+					if (splitUp.length > 1) {
+						cutBy = '</a>';
+						var split2 = splitUp[1].split(cutBy);
+						if (split2.length > 1) {
+							//console.log(split2[0]);
+							var split3 = split2[0].split('gsc_mp_anchor">');
+							//console.log(split3);
+							if (split3.length > 1) {
+								articleElements[4] = split3[1];
+								//console.log(split3[1]);
+							}
+						}
+					}
+					console.log('array value=' + articleElements[4]);
+					//alert("done1");
+
+				//}).done(function () {
+					chrome.storage.local.set({ 'articleValues': articleElements }, function () {
+						var error = chrome.runtime.lastError;
+						if (error) {
+							console.error(error);
+						}
+					});
+
+				//	var responseArray = Array();
+
+					responseArray[3] = articleElements;
+				//	//alert(articleElements[4]);
+					sendResponse(responseArray);
+				//	//alert("done2");
+				});
+			}
+
+			chrome.storage.local.set({ 'articleValues': articleElements }, function () {
+				var error = chrome.runtime.lastError;
+				if (error) {
+					console.error(error);
+				}
+			});
+
+			responseArray[3] = articleElements;
+			sendResponse(responseArray);
 		}
 	}
 );
--- popup.js
+++ popup.js
@@ -1,7 +1,5 @@
 //popup.js
-
-
-// JavaScript source code
+// open new google scholar search
 document.addEventListener('DOMContentLoaded', function () {
 	var newSearchButton = document.getElementById('newSearch');
 	newSearchButton.addEventListener('click', function () {
@@ -48,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	});
 }, false);
 
-// JavaScript source code
+// click tabs of advanced view
 document.addEventListener('DOMContentLoaded', function () {
 	var keywordTabElement = document.getElementById('keywordTab');
 	var authorTabElement = document.getElementById('authorTab');
@@ -88,12 +86,143 @@ document.addEventListener('DOMContentLoaded', function () {
 	});
 }, false);
 
+// swap between basic and advanced views
 document.addEventListener('DOMContentLoaded', function () {
-	chrome.storage.local.get('checkpage', function (data) {
-		var isActive = data.checkpage;
-		if (isActive) {
-			// reset the list of saved clickable items
-			isActive = false;
+	var advancedCheck = document.getElementById('advanced-int');
+	advancedCheck.addEventListener('click', function () {
+		if ($("#advanced-int").prop("checked") == true) {
+			// display the advanced section and hide the basic
+			$("#basic").removeClass("active");
+			$("#advanced").addClass("active");
+		}
+		else {
+			$("#advanced").removeClass("active");
+			$("#basic").addClass("active");
+		}
+		chrome.storage.local.set({ 'advCheckBox': $("#advanced-int").prop("checked") }, function () {
+			var error = chrome.runtime.lastError;
+			if (error) {
+				console.error(error);
+			}
+		});
+	});
+}, false);
+
+function finishedRefresh(val) {
+	$("#keywordList").empty();
+	$("#authorList").empty();
+	$("#affiliationList").empty();
+	$("#articleList").empty();
+	var authorArr = Array();
+	var keywordArr = Array();
+	var affiliationArr = Array();
+	var articleArr = Array();
+	var fullAuthor = "";
+
+	if (val !== undefined && val.length > 2) {
+		authorArr = val[0];
+		keywordArr = val[1];
+		affiliationArr = val[2];
+		articleArr = val[3];
+	}
+
+	for (var i = 0; keywordArr !== undefined && i < keywordArr.length; i = i + 2) {
+		var entry = document.createElement('div');
+		entry.classList.add("row");
+		var col1 = document.createElement('div');
+		col1.classList.add("twocolumn");
+		col1.innerHTML = '<input id="keyword' + i + '" name="keyword' + i + '" type="checkbox">' +
+			'<label for="keyword' + i + '">' + keywordArr[i] + '</label>';
+		entry.appendChild(col1);
+		var col2 = document.createElement('div');
+		col2.classList.add("twocolumn");
+		if (i + 1 < keywordArr.length) {
+			col2.innerHTML = '<input id="keyword' + (i + 1) + '" name="keyword' + (i + 1) + '" type="checkbox">' +
+				'<label for="keyword' + (i + 1) + '">' + keywordArr[(i + 1)] + '</label>';
+		}
+		entry.appendChild(col2);
+		$("#keywordList").append(entry);
+	}
+
+	for (var i = 0; authorArr !== undefined && i < authorArr.length; i = i + 2) {
+		var entry = document.createElement('div');
+		entry.classList.add("row");
+		var col1 = document.createElement('div');
+		col1.classList.add("twocolumn");
+		col1.innerHTML = '<input id="author' + i + '" name="author' + i + '" type="checkbox">' +
+			'<label for="author' + i + '">' + authorArr[i] + '</label>';
+		if (i == 0) {
+			fullAuthor = authorArr[i]
+		}
+		else {
+			fullAuthor = fullAuthor + ", " + authorArr[i];
+		}
+		entry.appendChild(col1);
+		var col2 = document.createElement('div');
+		col2.classList.add("twocolumn");
+		if (i + 1 < authorArr.length) {
+			col2.innerHTML = '<input id="author' + (i + 1) + '" name="author' + (i + 1) + '" type="checkbox">' +
+				'<label for="author' + (i + 1) + '">' + authorArr[(i + 1)] + '</label>';
+			fullAuthor = fullAuthor + ", " + authorArr[(i + 1)];
+		}
+		entry.appendChild(col2);
+		$("#authorList").append(entry);
+	}
+
+	for (var i = 0; affiliationArr !== undefined && i < affiliationArr.length; i = i + 2) {
+		var entry = document.createElement('div');
+		entry.classList.add("row");
+		var col1 = document.createElement('div');
+		col1.classList.add("twocolumn");
+		col1.innerHTML = '<input id="affiliation' + i + '" name="affiliation' + i + '" type="checkbox">' +
+			'<label for="affiliation' + i + '">' + affiliationArr[i] + '</label>';
+		entry.appendChild(col1);
+		var col2 = document.createElement('div');
+		col2.classList.add("twocolumn");
+		if (i + 1 < affiliationArr.length) {
+			col2.innerHTML = '<input id="affiliation' + (i + 1) + '" name="affiliation' + (i + 1) + '" type="checkbox">' +
+				'<label for="affiliation' + (i + 1) + '">' + affiliationArr[(i + 1)] + '</label>';
+		}
+		entry.appendChild(col2);
+		$("#affiliationList").append(entry);
+	}
+
+	var entry = document.createElement('div');
+	entry.style.padding = "8px";
+	// Publication Type (ie CONFERENCE)
+	var enChild = document.createElement('div');
+	enChild.style.fontColor = "grey";
+	enChild.innerHTML = articleArr[2]
+	entry.appendChild(enChild);
+	// Paper title
+	enChild = document.createElement('div');
+	enChild.style.fontWeight = "bold";
+	enChild.innerHTML = articleArr[0];
+	entry.appendChild(enChild);
+	// Authors
+	enChild = document.createElement('div');
+	enChild.innerHTML = fullAuthor;
+	entry.appendChild(enChild);
+	// Publication
+	enChild = document.createElement('div');
+	enChild.style.fontStyle = "italic";
+	enChild.innerHTML = articleArr[1];
+	//alert(articleArr[4]);
+	if (articleArr[4] > 0) {
+		enChild.innerHTML = articleArr[1] + " [h5-index: " + articleArr[4] + "]";
+	}
+	else {
+		enChild.innerHTML = articleArr[1];
+	}
+	entry.appendChild(enChild);
+	$("#articleList").append(entry);
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	var refreshBtn = document.getElementById('refresh_list');
+	refreshBtn.addEventListener('click', function () {
+		chrome.storage.local.get('checkpage', function (data) {
 			chrome.storage.local.clear(function () {
 				var error = chrome.runtime.lastError;
 				if (error) {
@@ -103,28 +232,54 @@ document.addEventListener('DOMContentLoaded', function () {
 			// Send a message to the active tab to collect the list of clickable items
 			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 				var activeTab = tabs[0];
-				chrome.tabs.sendMessage(activeTab.id, { "message": "gather_list_of_items" });
+				chrome.tabs.sendMessage(activeTab.id, { "message": "gather_list_of_items" }, finishedRefresh);
 			});
-		}
-		else {
-			isActive = true;
+			chrome.storage.local.set({ 'checkpage': true });
+			chrome.storage.local.set({ 'advCheckBox': $("#advanced-int").prop("checked") }, function () {
+				var error = chrome.runtime.lastError;
+				if (error) {
+					console.error(error);
+				}
+			});
+		});
 
-			// Send a message to the active tab to collect the list of clickable items
-			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-				var activeTab = tabs[0];
-				chrome.tabs.sendMessage(activeTab.id, { "message": "gather_list_of_items" });
-			});
-		}
-		chrome.storage.local.set({ 'checkpage': isActive });
+		//setFields();
 	});
 }, false);
 
+document.addEventListener('DOMContentLoaded', function () {
+	chrome.storage.local.get('checkpage', function (data) {
+		var isActive = data.checkpage;
+		// !== undefined
+		if (isActive === undefined || isActive === false) {
+			// reset the list of saved clickable items
+			isActive = true;
+			chrome.storage.local.clear(function () {
+				var error = chrome.runtime.lastError;
+				if (error) {
+					console.error(error);
+				}
+			});
+			// Send a message to the active tab to collect the list of clickable items
+			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+				var activeTab = tabs[0];
+				chrome.tabs.sendMessage(activeTab.id, { "message": "gather_list_of_items" }, finishedRefresh);
+			});
+			chrome.storage.local.set({ 'checkpage': isActive });
+		}
+	});
+}, false);
 
-window.onload = function () {
+function setFields() {
+	$("#keywordList").empty();
+	$("#authorList").empty();
+	$("#affiliationList").empty();
+	$("#articleList").empty();
+	var fullAuthor = "";
+
 	chrome.storage.local.get('keywordValues', function (arr) {
 		var keywordArr = arr.keywordValues;
-		for (var i = 0; keywordArr !== undefined && i < keywordArr.length; i = i + 2)
-		{
+		for (var i = 0; keywordArr !== undefined && i < keywordArr.length; i = i + 2) {
 			var entry = document.createElement('div');
 			entry.classList.add("row");
 			var col1 = document.createElement('div');
@@ -152,15 +307,98 @@ window.onload = function () {
 			col1.classList.add("twocolumn");
 			col1.innerHTML = '<input id="author' + i + '" name="author' + i + '" type="checkbox">' +
 				'<label for="author' + i + '">' + authorArr[i] + '</label>';
+			if (i == 0) {
+				fullAuthor = authorArr[i]
+			}
+			else {
+				fullAuthor = fullAuthor + ", " + authorArr[i];
+			}
 			entry.appendChild(col1);
 			var col2 = document.createElement('div');
 			col2.classList.add("twocolumn");
 			if (i + 1 < authorArr.length) {
 				col2.innerHTML = '<input id="author' + (i + 1) + '" name="author' + (i + 1) + '" type="checkbox">' +
 					'<label for="author' + (i + 1) + '">' + authorArr[(i + 1)] + '</label>';
+				fullAuthor = fullAuthor + ", " + authorArr[(i + 1)];
 			}
 			entry.appendChild(col2);
 			$("#authorList").append(entry);
+		}
+	});
+
+	chrome.storage.local.get('affiliationValues', function (arr) {
+		var affiliationArr = arr.affiliationValues;
+		for (var i = 0; affiliationArr !== undefined && i < affiliationArr.length; i = i + 2) {
+			var entry = document.createElement('div');
+			entry.classList.add("row");
+			var col1 = document.createElement('div');
+			col1.classList.add("twocolumn");
+			col1.innerHTML = '<input id="affiliation' + i + '" name="affiliation' + i + '" type="checkbox">' +
+				'<label for="affiliation' + i + '">' + affiliationArr[i] + '</label>';
+			entry.appendChild(col1);
+			var col2 = document.createElement('div');
+			col2.classList.add("twocolumn");
+			if (i + 1 < affiliationArr.length) {
+				col2.innerHTML = '<input id="affiliation' + (i + 1) + '" name="affiliation' + (i + 1) + '" type="checkbox">' +
+					'<label for="affiliation' + (i + 1) + '">' + affiliationArr[(i + 1)] + '</label>';
+			}
+			entry.appendChild(col2);
+			$("#affiliationList").append(entry);
+		}
+	});
+
+	chrome.storage.local.get('articleValues', function (arr) {
+		var articleArr = arr.articleValues;
+		var entry = document.createElement('div');
+		entry.style.padding = "8px";
+		var enChild;
+
+		// Publication Type (ie CONFERENCE)
+		enChild = document.createElement('div');
+		enChild.style.fontColor = "grey";
+		enChild.innerHTML = articleArr[2];
+		entry.appendChild(enChild);
+		// Paper title
+		enChild = document.createElement('div');
+		enChild.style.fontWeight = "bold";
+		enChild.innerHTML = articleArr[0];
+		entry.appendChild(enChild);
+		// Authors
+		enChild = document.createElement('div');
+		enChild.innerHTML = fullAuthor;
+		entry.appendChild(enChild);
+		// Publication title
+		enChild = document.createElement('div');
+		enChild.style.fontStyle = "italic";
+		if (articleArr[4] > 0) {
+			enChild.innerHTML = articleArr[1] + " [h5-index: " + articleArr[4] + "]";
+		}
+		else {
+			enChild.innerHTML = articleArr[1];
+		}
+
+		entry.appendChild(enChild);
+		$("#articleList").append(entry);
+	});
+}
+
+window.onload = function () {
+	setFields();
+
+	chrome.storage.local.get('advCheckBox', function (val) {
+		if (val.advCheckBox !== undefined) {
+			var isChecked = val.advCheckBox;
+
+			$("#advanced-int").prop("checked", isChecked);
+			if (isChecked === true) {
+				// display the advanced section and hide the basic
+				$("#basic").removeClass("active");
+				$("#advanced").addClass("active");
+			}
+			else {
+				$("#advanced").removeClass("active");
+				$("#basic").addClass("active");
+			}
 		}
 	});
 };
